4bdc9fb43e9831d6da1244a8a8255a09bdab5cc2
vinumaradana <127369782+vinumaradana@users.noreply.github.com>
2023-08-29 18:13:38 -0700

    changed styling

--- filter_project_db.php
+++ filter_project_db.php
@@ -4,147 +4,236 @@
 
 <!DOCTYPE html>
 <html>
-<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-	<title>Filtering Projects</title>
-	<style>
-		body {
-			margin-top: 20px; 
-            display: flex;
-			justify-content: space-between;
-		}
-        .filter-section {
-			flex: 1;
-			margin-right: 10px;
-            margin-bottom: 20px;
-		}
-        .results-section {
-			flex: 2;
-            margin-top: 20px;
-		}
-		table {
-			border-collapse: collapse;
-			margin: 10 auto;
-			width: 90%;
-		}
-		th, td {
-			border: 1px solid black;
-			padding: 8px;
-			text-align: left;
-            font-size: 14px; 
-		}
-		th {
-			background-color: #f2f2f2;
-		}
-        input[type="submit"]{
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            cursor: pointer;
-        }
-        .checkbox-group {
-            display: flex;
-            flex-wrap: wrap;
-        }
-        .checkbox-label {
-            display: flex;
-            align-items: center;
-            margin-right: 10px;
-        }
-        
-        /* Style for the search bar container */
-        #search {
-            margin-top: 10px;
-            display: block;
-            font-size: 14px;
-            font-family: Verdana;
-        }
-        
-        /* Style for the search input */
-        #search {
-            width: 80%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
-            transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-        }
-        
-        /* Style for the search input on focus */
-        #search:focus {
-            border-color: #66afe9;
-            outline: 0;
-            box-shadow: 0 0 5px rgba(102, 175, 233, 0.5);
-        }
-
-
-	</style>
-    
-</head>
 <body>
 
-<div class="filter-section">
+<style>
+  /* Define a custom class with the desired background color */
+  .custom-btn {
+    background-color: #28257e;
+    color: #fff; /* Optional: Set text color for better contrast */
+  }
+  .bold-with-padding {
+    font-weight: bold;
+    padding-top: 10px;
+  }
+  .custom-heading {
+    color: #28257e;
+  }
+    /* Style the table borders */
+  .custom-table {
+    border: 3px solid #28257e;
+  }
+  
+  /* Style the table header borders */
+  .custom-table th {
+    border: 3px solid #28257e;
+  }
+
+  /* Style the table cell borders */
+  .custom-table td {
+    border: 3px solid #28257e;
+  }
+</style>
+
+<div class="container mt-3">
     <form method="post" action="">
-        <h2>Filtering Project Database</h2>
-        <p>Major</p>
-        <div class="checkbox-group">
-            
-            <label><input type="checkbox" name="major[]" value="Biology"> Biology</label>
-            <label><input type="checkbox" name="major[]" value="Chemistry"> Chemistry</label>
-            <label><input type="checkbox" name="major[]" value="Computer Science"> Computer Science</label>
-            <label><input type="checkbox" name="major[]" value="Bioengineering"> Bioengineering</label>
-            <label><input type="checkbox" name="major[]" value="Bioinformatics"> Bioinformatics</label>
-            <label><input type="checkbox" name="major[]" value="Data Science"> Data Science</label>
-            <label><input type="checkbox" name="major[]" value="Cognitive Science"> Cognitive Science</label>
-            <label><input type="checkbox" name="major[]" value="Computer Engineering"> Computer Engineering</label>
-            <label><input type="checkbox" name="major[]" value="Business/Economics"> Business/Economics</label>
-            <label><input type="checkbox" name="major[]" value="Electrical Engineering"> Electrical Engineering</label>
-            <label><input type="checkbox" name="major[]" value="Physics"> Physics</label>
-            <label><input type="checkbox" name="major[]" value="Mechanical Engineering"> Mechanical Engineering</label>
-            <label><input type="checkbox" name="major[]" value="Aerospace Engineering"> Aerospace Engineering</label>
-            <label><input type="checkbox" name="major[]" value="NanoEngineering"> NanoEngineering</label>
-            <label><input type="checkbox" name="major[]" value="Structural Engineering"> Structural Engineering</label>
-            <label><input type="checkbox" name="major[]" value="Global/Public Health"> Global/Public Health</label>
-            <label><input type="checkbox" name="major[]" value="Mathematics"> Mathematics</label>
-            <label><input type="checkbox" name="major[]" value="Probability&Statistics"> Probability&Statistics</label>
-            <label><input type="checkbox" name="major[]" value="Psychology"> Psychology</label>
-            <label><input type="checkbox" name="major[]" value="Sociology"> Sociology</label>
-            <label><input type="checkbox" name="major[]" value="Political Science"> Political Science</label>
+        <h2 class = "custom-heading">Filtering Project Database</h2>
+        <p class = "bold-with-padding">Major</p>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Biology"> Biology</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Chemistry"> Chemistry</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check"> 
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Computer Science"> CS</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Bioengineering"> Bioeng</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Bioinformatics"> Bioinformatics</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Data Science"> Data Science</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Cognitive Science"> Cog Sci</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Computer Engineering"> Computer Eng</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Business/Economics"> Business/Econ</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Electrical Engineering"> Electrical Eng</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Physics"> Physics</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Mechanical Engineering"> Mechanical Eng</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Aerospace Engineering"> Aerospace Eng</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="NanoEngineering"> NanoEng</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Structural Engineering"> Structural Eng</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Global/Public Health"> Public Health</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Mathematics"> Mathematics</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Probability&Statistics"> Prob&Stats</label>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Psychology"> Psychology</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Sociology"> Sociology</label>
+                </div>
+            </div>   
+            <div class="col-md-3">
+                <div class="form-check">
+                        <label><input class="form-check-input" type="checkbox" name="major[]" value="Political Science"> Poli Sci</label>
+                </div>
+            </div>   
         </div>
 
 
-        <p>Education</p>
-        <div class="checkbox-group">
-            <label><input type="checkbox" name="education[]" value="undergraduate">undergraduate</label>
-            <label><input type="checkbox" name="education[]" value="graduate"> graduate</label>
+        <p class = "bold-with-padding">Education</p>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="education[]" value="undergraduate">undergraduate</label>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="education[]" value="graduate"> graduate</label>
+                </div>
+            </div>
         </div>
         
         
-        <p>Time Commitment (hours/week)</p>
-        <div class="checkbox-group">
-            <label><input type="checkbox" name="time[]" value="5">5</label>
-            <label><input type="checkbox" name="time[]" value="10">10</label>
-            <label><input type="checkbox" name="time[]" value="15">15</label>
-            <label><input type="checkbox" name="time[]" value="20">20</label>
-            <label><input type="checkbox" name="time[]" value="25">25</label>
-            <label><input type="checkbox" name="time[]" value="30">30</label>
-            <label><input type="checkbox" name="time[]" value="35">35</label>
-            <label><input type="checkbox" name="time[]" value="40">40</label>
+        <p class = "bold-with-padding">Time Commitment (hours/week)</p>
+        <div class="row">
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="5">5</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="10">10</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="15">15</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="20">20</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="25">25</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="30">30</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="35">35</label>
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="form-check">
+                    <label><input class="form-check-input" type="checkbox" name="time[]" value="40">40</label>
+                </div>
+            </div>
         </div>
 
         <br>
-        <div class="checkbox-group">
+        <div class = "bold-with-padding">
             <label for="search"></label>
             <input type="text" id="search" name="search" placeholder="Enter keywords...">
         </div>
 
         <br>
-        <input type="submit" name="apply_filter" value="Apply Filter">
+        <input class="btn custom-btn mt-2" type="submit" name="apply_filter" value="Apply Filter">
     </form>
 </div>
 
-<div class="results-section">
+<div class="container mt-5">
     <?php
         # showing database data on a website using php
         $sql = "SELECT * FROM project_info;";
@@ -162,8 +251,8 @@
 
         # if there are results from the query
         if($resultCheck > 0){
-            echo '<h2>Current Database</h2>';
-            echo '<table>';
+            echo '<h2 class = "custom-heading">Current Database</h2>';
+            echo '<table class="table custom-table">';
             echo '<tr><th>Name</th><th>Email</th><th>Title</th><th>Description</th><th>Length</th><th>Students</th><th>Major</th><th>Education</th><th>Time</th><th>Skills</th></tr>';
 
             # spit out data as long as there are more results
@@ -227,20 +316,17 @@
                     # Loop through array and add each individual word into conditions
                     $splitSearch = preg_split('/[\s,]+/', $filterSearch);
                     $filterConditions[sizeof($splitSearch)];
-                    print_r($splitSearch);
                     for ($x = 0; $x < sizeof($splitSearch); $x++) {
                         $filteredSearch = $splitSearch[$x];
                         $filterConditions[$x] = "(email LIKE '%$filteredSearch%' OR name LIKE '%$filteredSearch%' OR workexp LIKE '%$filteredSearch%' OR interest LIKE '%$filteredSearch%')";
                     }
                     $filteredConditions .= implode(' OR ', $filterConditions);
-                    print_r($filteredConditions);
                     $conditions[] = "($filteredConditions)";
                 }
 
                 
                 $sql .= implode(' AND ', $conditions);
                 
-
                 # executes filter query and stores result
                 $filterResult = mysqli_query($conn, $sql);
 
@@ -248,8 +334,8 @@
                     $filterResultCheck = mysqli_num_rows($filterResult);
 
                     if ($filterResultCheck > 0) {
-                        echo '<h2>Filtered Results</h2>';
-                        echo '<table>';
+                        echo '<h2 class = "custom-heading">Filtered Results</h2>';
+                        echo '<table class="table custom-table">';
                         echo '<tr><th>Name</th><th>Email</th><th>Title</th><th>Description</th><th>Length</th><th>Students</th><th>Major</th><th>Education</th><th>Time</th><th>Skills</th></tr>';
 
                         # spit out data as long as there are more results
