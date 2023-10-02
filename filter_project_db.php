1e7ebc223ccc6bac07fa45ef7f6d3d93c29623e6
NumaanFormoli <74218473+NumaanFormoli@users.noreply.github.com>
2023-10-02 09:21:33 -0700

    Update filter_project_db.php with DataTables

--- filter_project_db.php
+++ filter_project_db.php
@@ -4,39 +4,50 @@
 
 <!DOCTYPE html>
 <html>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
+        <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+        <script defer src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+        <script defer src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+        
+        <script defer src="js/DataTable.js"></script> 
+        <script defer src="js/readMore.js"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <style>
+          /* Define a custom class with the desired background color */
+          .custom-btn {
+            background-color: #28257e;
+            color: #fff; /* Optional: Set text color for better contrast */
+          }
+          .bold-with-padding {
+            font-weight: bold;
+            padding-top: 10px;
+          }
+          .custom-heading {
+            color: #28257e;
+          }
+            /* Style the table borders */
+          .custom-table {
+            border: 3px solid #28257e;
+          }
+          
+          /* Style the table header borders */
+          .custom-table th {
+            border: 3px solid #28257e;
+          }
+        
+          /* Style the table cell borders */
+          .custom-table td {
+            border: 3px solid #28257e;
+          }
+        </style>
+    </head>
 
 <body>
-
-<style>
-  /* Define a custom class with the desired background color */
-  .custom-btn {
-    background-color: #28257e;
-    color: #fff; /* Optional: Set text color for better contrast */
-  }
-  .bold-with-padding {
-    font-weight: bold;
-    padding-top: 10px;
-  }
-  .custom-heading {
-    color: #28257e;
-  }
-    /* Style the table borders */
-  .custom-table {
-    border: 3px solid #28257e;
-  }
-  
-  /* Style the table header borders */
-  .custom-table th {
-    border: 3px solid #28257e;
-  }
-
-  /* Style the table cell borders */
-  .custom-table td {
-    border: 3px solid #28257e;
-  }
-</style>
-
 <div class="container mt-3">
     <form method="post" action="">
         <h2 class = "custom-heading">Filtering Project Database</h2>
@@ -229,11 +240,11 @@
         </div>
 
         <br>
-        <input class="btn custom-btn mt-2" type="submit" name="apply_filter" value="Apply Filter">
+        <input class="btn custom-btn mt-2" type="submit" name="apply_filter" id="apply_filter" value="Apply Filter">
     </form>
 </div>
 
-<div class="container mt-5">
+<div class="container-fluid mt-5">
     <?php
         # showing database data on a website using php
         $sql = "SELECT * FROM project_info;";
@@ -252,26 +263,42 @@
         # if there are results from the query
         if($resultCheck > 0){
             echo '<h2 class = "custom-heading">Current Database</h2>';
-            echo '<table class="table custom-table">';
-            echo '<tr><th>Name</th><th>Email</th><th>Title</th><th>Description</th><th>Length</th><th>Students</th><th>Major</th><th>Education</th><th>Time</th><th>Skills</th></tr>';
+            echo '<table id="example" class="table table-bordered table-hover" style="width:100%">';
+            echo '
+            <thead class="table-group-divider">
+            <tr>
+            <th class="col-sm-1">Name</th>
+            <th class="col-sm-1">Email</th>
+            <th class="col-sm-1">Title</th>
+            <th class="col-sm-3">Description</th>
+            <th>Length</th>
+            <th>Students</th>
+            <th class="col-sm-1">Major</th>
+            <th>Education</th>
+            <th>Time</th>
+            <th class="col-sm-3">Skills</th>
+            </tr>
+            </thead>';
 
+            echo '<tbody">';
             # spit out data as long as there are more results
             while ($row = mysqli_fetch_assoc($result)){
-                echo '<tr>';
+                echo '<tr class="description-row">';
                 echo '<td>', $row['name'], '</td>';
                 echo '<td>', $row['email'], '</td>';
                 echo '<td>', $row['title'], '</td>';
-                echo '<td>', $row['description'], '</td>';
+                echo '<td class="description ellipsis"><p class="description ellipsis">', $row['description'], '</p></td>';
                 echo '<td>', $row['length'], '</td>';
                 echo '<td>', $row['students'], '</td>';
                 echo '<td>', $row['major'], '</td>';
                 echo '<td>', $row['education'], '</td>';
                 echo '<td>', $row['time'], '</td>';
-                echo '<td>', $row['skills'], '</td>';
+                echo '<td class="description ellipsis"><p class="description ellipsis">', $row['skills'], '</p></td>';
 
                 echo '</tr>';
             }
 
+            echo '</tbody>';
             echo '</table>';
         
 
@@ -318,7 +345,7 @@
                     $filterConditions[sizeof($splitSearch)];
                     for ($x = 0; $x < sizeof($splitSearch); $x++) {
                         $filteredSearch = $splitSearch[$x];
-                        $filterConditions[$x] = "(email LIKE '%$filteredSearch%' OR name LIKE '%$filteredSearch%' OR workexp LIKE '%$filteredSearch%' OR interest LIKE '%$filteredSearch%')";
+                        $filterConditions[$x] = "(name LIKE '%$filteredSearch%' OR email LIKE '%$filteredSearch%' OR title LIKE '%$filteredSearch%' OR description LIKE '%$filteredSearch%' OR skills LIKE '%$filteredSearch%' )";
                     }
                     $filteredConditions .= implode(' OR ', $filterConditions);
                     $conditions[] = "($filteredConditions)";
@@ -334,26 +361,43 @@
                     $filterResultCheck = mysqli_num_rows($filterResult);
 
                     if ($filterResultCheck > 0) {
-                        echo '<h2 class = "custom-heading">Filtered Results</h2>';
-                        echo '<table class="table custom-table">';
-                        echo '<tr><th>Name</th><th>Email</th><th>Title</th><th>Description</th><th>Length</th><th>Students</th><th>Major</th><th>Education</th><th>Time</th><th>Skills</th></tr>';
-
+                        echo '<h2 class = "custom-heading">Current Database</h2>';
+                        echo '<table id="example" class="table table-bordered table-hover" style="width:100%">';
+                        echo '
+                        <thead class="table-group-divider">
+                        <tr>
+                        <th class="col-sm-1">Name</th>
+                        <th class="col-sm-1">Email</th>
+                        <th class="col-sm-1">Title</th>
+                        <th class="col-sm-3">Description</th>
+                        <th>Length</th>
+                        <th>Students</th>
+                        <th class="col-sm-1">Major</th>
+                        <th>Education</th>
+                        <th>Time</th>
+                        <th class="col-sm-3">Skills</th>
+                        </tr>
+                        </thead>';
+            
                         # spit out data as long as there are more results
+                        echo '<tbody">';
                         while ($row = mysqli_fetch_assoc($filterResult)){
-                            echo '<tr>';
+                            echo '<tr class="description-row">';
                             echo '<td>', $row['name'], '</td>';
                             echo '<td>', $row['email'], '</td>';
                             echo '<td>', $row['title'], '</td>';
-                            echo '<td>', $row['description'], '</td>';
+                            echo '<td class="description ellipsis"><p class="description ellipsis">', $row['description'], '</p></td>';
                             echo '<td>', $row['length'], '</td>';
                             echo '<td>', $row['students'], '</td>';
                             echo '<td>', $row['major'], '</td>';
                             echo '<td>', $row['education'], '</td>';
                             echo '<td>', $row['time'], '</td>';
-                            echo '<td>', $row['skills'], '</td>';
+                            echo '<td class="description ellipsis"><p class="description ellipsis">', $row['skills'], '</p></td>';
+            
                             echo '</tr>';
                         }
-
+            
+                        echo '</tbody>';
                         echo '</table>';
                         
                         
